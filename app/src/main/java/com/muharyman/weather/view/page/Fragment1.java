442c39d749358509cd4ef7b0b3f69f9598179589
muharyman <muharymank@gmail.com>
2019-11-10 15:40:01 +0700

    inisiasi view

--- app/src/main/java/com/muharyman/weather/view/page/Fragment1.java
+++ app/src/main/java/com/muharyman/weather/view/page/Fragment1.java
@@ -1,4 +1,25 @@
 package com.muharyman.weather.view.page;
 
-public class Fragment1 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.muharyman.weather.view.base.BaseFragment;
+
+public class Fragment1 extends BaseFragment implements Contract{
+    @Override
+    protected View onBaseCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void updateData() {
+
+    }
+
+    @Override
+    public void onFailure(String body) {
+
+    }
 }
--- app/src/main/java/com/muharyman/weather/view/page/MainActivity.java
+++ app/src/main/java/com/muharyman/weather/view/page/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.muharyman.weather.R;
+import com.muharyman.weather.view.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity  implements Contract{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void updateData() {
+
+    }
+
+    @Override
+    public void onFailure(String body) {
+
     }
 }
