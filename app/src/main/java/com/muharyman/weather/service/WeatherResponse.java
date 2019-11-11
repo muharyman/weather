21e870adb79479efa0f3bace316c2a965778db81
muharyman <muharymank@gmail.com>
2019-11-11 17:49:09 +0700

    fixing response

--- app/src/main/java/com/muharyman/weather/service/WeatherResponse.java
+++ app/src/main/java/com/muharyman/weather/service/WeatherResponse.java
@@ -1,28 +1,34 @@
-package com.muharyman.weather.service.service;
+package com.muharyman.weather.service;
 
-import com.google.gson.annotations.SerializedName;
 import com.muharyman.weather.model.Clouds;
 import com.muharyman.weather.model.Coord;
-import com.muharyman.weather.model.ListWeather;
 import com.muharyman.weather.model.Main;
 import com.muharyman.weather.model.Rain;
+import com.muharyman.weather.model.Snow;
 import com.muharyman.weather.model.Sys;
+import com.muharyman.weather.model.Weather;
 import com.muharyman.weather.model.Wind;
 
+import java.util.Date;
+
 import io.realm.Realm;
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.RealmResults;
+import io.realm.Sort;
 import io.realm.annotations.Index;
 import io.realm.annotations.PrimaryKey;
 
 public class WeatherResponse extends RealmObject {
     public Coord coord;
-    @SerializedName("weather")
-    public ListWeather listWeather;
+    public RealmList<Weather> weather;
     public String base;
     public Main main;
     public Wind wind;
     public Rain rain;
+    public Snow snow;
+    public long dt;
+    public long visibility;
     public Clouds clouds;
     public Sys sys;
     public int timezone;
@@ -31,12 +37,13 @@ public class WeatherResponse extends RealmObject {
     public long id;
     public String name;
     public int cod;
+    public Date date;
 
     public WeatherResponse(){
         super();
     }
 
-    public static RealmResults<WeatherResponse> getQuery(Realm realm, String search_key){
-        return realm.where(WeatherResponse.class).findAllAsync();
+    public static RealmResults<WeatherResponse> getQuery(Realm realm){
+        return realm.where(WeatherResponse.class).sort("date", Sort.DESCENDING).findAllAsync();
     }
 }
