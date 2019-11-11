71fa4f94b01dc9295b75532fcda820235413cc1c
muharyman <muharymank@gmail.com>
2019-11-11 22:45:19 +0700

    inisiasi unitTest

--- app/src/test/java/com/muharyman/weather/ExampleUnitTest.java
+++ app/src/test/java/com/muharyman/weather/ExampleUnitTest.java
@@ -1,5 +1,11 @@
 package com.muharyman.weather;
 
+import androidx.fragment.app.Fragment;
+
+import com.muharyman.weather.view.page.Adapter;
+import com.muharyman.weather.view.page.Fragment1;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,8 +16,19 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void kelvintoCelcius(){
+        Fragment1 fragment1 = new Fragment1();
+        int celcius = fragment1.convertKelvinToCelcius(293);
+        Assert.assertTrue(celcius==20);
+    }
+
+
 }
