740c273d27bff94c0db891f1e677d627ea7bc4bc
muharyman <muharymank@gmail.com>
2019-11-11 19:26:33 +0700

    add request permission

--- app/src/main/java/com/muharyman/weather/App.java
+++ app/src/main/java/com/muharyman/weather/App.java
@@ -13,6 +13,7 @@ public class App extends MultiDexApplication {
 
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
     public static final String APPID = "9fb6176409645f92abc6d663b51e379a";
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     @Override
     public void onCreate() {
